de9dcf9e8ae86096c758da575f52c50067bc9108
🐊Baligator🐊 <79498281+balintfritz@users.noreply.github.com>
2024-04-08 19:02:43 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+lua54 'yes'
+
+auth '_baligator_ & aka_mono'
+
+client_script {
+    'client/*.lua'
+}
+
+shared_script {
+    'shared/*.lua',
+    '@ox_lib/init.lua'
+}
+
+server_script {
+    'server/*.lua'
+}
